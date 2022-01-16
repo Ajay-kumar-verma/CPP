57638c8a093927805f793e85a9992e97a00abc07
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-01-16 23:49:17 +0530

    reference and pointer variable

--- refernve_val.cpp
+++ refernve_val.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
-
 using namespace std;
 
+ void swap1(int a,int b){
+  a+=b;
+  b=a-b;
+  a=a-b; 
+ } 
 
-int sum(int a,int b){
-    int c= a+b;
-     return c;
-     }
+void swap2(int & a,int & b){
+  a+=b;
+  b=a-b;
+  a=a-b; 
+ } 
+
+
+void swap3(int *a,int *b){
+  int temp=*a;
+   *a=*b;
+  *b=temp;
+ 
+   } 
+
 
 
 int main(){
 
- cout<<sum(3,5);
+  cout<<"a = ";
+  int a;
+  cin>>a;
+
+  cout<<"b = ";
+  int b;
+  cin>>b;
+
+  cout<<"Before swapped : "<<"\n";
+  cout<<"a = "<<a<<"\n";
+  cout<<"b = "<<b<<"\n";
+
+  //  swap1(a,b); 
+   swap2(a,b);
+  
+  cout<<"After swapped : "<<"\n";
+  cout<<"a = "<<a<<"\n";
+  cout<<"b = "<<b<<"\n";
+
+
 
     return 0;
 }
